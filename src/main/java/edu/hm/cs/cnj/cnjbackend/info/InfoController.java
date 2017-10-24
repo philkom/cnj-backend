3412a35893ccdfd92608c6f73c0fd05cb058c655
Philipp Komninos <philkom@gmx.de>
2017-10-24 16:21:48 +0200

    Take info message as value from property file or env. variable

--- src/main/java/edu/hm/cs/cnj/cnjbackend/info/InfoController.java
+++ src/main/java/edu/hm/cs/cnj/cnjbackend/info/InfoController.java
@@ -1,15 +1,19 @@
 package edu.hm.cs.cnj.cnjbackend.info;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class InfoController {
 	
+	@Value(value = "${cnj.info.message}")
+	String message;
+	
 	@RequestMapping("/info")
 	public Info greeting() {
 		Info result = new Info();
-		result.setMessage("OK");
+		result.setMessage(message);
 		return result;
 	}
 }
