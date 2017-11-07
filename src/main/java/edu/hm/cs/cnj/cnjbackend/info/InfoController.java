eb04ba90997566c37e6c3e1fa24d1d00f8406811
Philipp Komninos <philkom@gmx.de>
2017-11-07 14:47:54 +0100

    Added CF Manifest file. Changed properties to yml format. Added support for configuration server with profiles dev and cloud

--- src/main/java/edu/hm/cs/cnj/cnjbackend/info/InfoController.java
+++ src/main/java/edu/hm/cs/cnj/cnjbackend/info/InfoController.java
@@ -1,19 +1,19 @@
 package edu.hm.cs.cnj.cnjbackend.info;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class InfoController {
 	
-	@Value(value = "${cnj.info.message}")
-	String message;
+	@Autowired
+	InfoProperties infoProperties;
 	
 	@RequestMapping("/info")
 	public Info greeting() {
 		Info result = new Info();
-		result.setMessage(message);
+		result.setMessage(infoProperties.getMessage());
 		return result;
 	}
 }
