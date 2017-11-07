9ab08dd97fc57ce53228d2415e449f313e8539c4
Philipp Komninos <philkom@gmx.de>
2017-11-07 20:35:38 +0100

    Added integration test for InfoController

---
+++ src/test/java/edu/hm/cs/cnj/cnjbackend/info/InfoControllerIntegrationTest.java
@@ -0,0 +1,31 @@
+package edu.hm.cs.cnj.cnjbackend.info;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class InfoControllerIntegrationTest {
+
+	@MockBean
+	InfoProperties infoProperties;
+
+	@Autowired
+	InfoController serviceUnderTest;
+	
+	private final String message = "TestMessage";
+	
+	@Test
+	public void testInfoMessage() {
+		given(this.infoProperties.getMessage()).willReturn(message);
+		Info info = serviceUnderTest.greeting();
+		assertThat(info.getMessage()).isEqualTo(message);
+	}
+}
